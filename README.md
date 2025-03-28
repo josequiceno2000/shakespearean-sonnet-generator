3fb3b778f2da64405c6e9ee6536da2e05bc531d9
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-28 13:59:39 -0500

    Get specific line number based on input

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Try to get user to input a theme and generate verses related to it.
 
 ## Steps
 1. ~~Figure out how to get an input as a cli tool~~ ✔
+2. ~~Figure out how to run it in terminal with a command~~ ✔
+3. ~~Get the second argument to be the number of lines printed~~ ✔
--- main.go
+++ main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
-	"bufio"
+	"strconv"
 ) 
 
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ssg <name>")
+		os.Exit(1)
+	}
+	arg1 := os.Args[1]
 
-	fmt.Print("SSG > ")
-	scanner.Scan()
-	name := scanner.Text()
-	fmt.Println(name)
+	maxLength, err := strconv.Atoi(arg1)
+	if err != nil {
+		fmt.Printf("Error: '%s' is not a valid integer.\n", arg1)
+		os.Exit(1)
+	}
+	
+	for i := 1; i <= maxLength; i++ {
+		fmt.Printf("%v. When I do count the clock that tells the time\n", i)
+	}
+	
 }
